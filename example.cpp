6c7cbf673ce0b99802e4291e07c154504f3db754
Arco <49058858+odakade@users.noreply.github.com>
2020-02-18 15:33:29 +0700

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,30 @@
+#ifde __APLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void display(){
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+	glClear(GL_COLOR_BUFFER_BIT);
+
+glBegin(GL_QUADS);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(-1.0f, -1.0f);
+	glVertex2f(1.0f, -1.0f);
+	glVertex2f(1.0f, 1.0f);
+	glVertex2f(-1.0f, 1.0f);
+glEnd();
+glFlush();
+}
+
+int main (int argc, char** argv){
+	glutInit(&argc, argv);
+	glutCreateWindow("Program OpenGL");
+	glutInitWindowSize(320,320);
+	glutInitWindowPosition(50,50);
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
