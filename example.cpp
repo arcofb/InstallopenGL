7ed103464a402b1ffe82a29958f21607aef68bd9
Arco <49058858+arcofb@users.noreply.github.com>
2020-02-28 16:05:32 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,4 +1,4 @@
-#ifde __APLE__
+#ifdef __APLE__
 #include <OpenGL/OpenGL.h>
 #include <GLUT/glut.h>
 #else
@@ -6,15 +6,15 @@
 #endif
 
 void display(){
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 glBegin(GL_QUADS);
 	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex2f(-1.0f, -1.0f);
-	glVertex2f(1.0f, -1.0f);
-	glVertex2f(1.0f, 1.0f);
-	glVertex2f(-1.0f, 1.0f);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
 glEnd();
 glFlush();
 }
